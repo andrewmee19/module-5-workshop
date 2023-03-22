7d0376525c3ee6e35f3de2e7cffa57df1e547629
Andrew Mee <suemeejim715@gmail.com>
2023-03-22 18:31:47 -0400

    Add FizzBuzz Logic and Tests

--- FizzBuzz.Core/FizzBuzzService.cs
+++ FizzBuzz.Core/FizzBuzzService.cs
@@ -1,5 +1,28 @@
 ﻿namespace FizzBuzz.Core;
 public class FizzBuzzService
 {
+    public string Print(int num)
+    {
+        if (IsDivisibleByThree(num) && IsDivisibleByFive(num)) {
+            return "FizzBuzz";
+        }
+        else if (IsDivisibleByThree(num)) {
+            return "Fizz";
+        }
+        else if (IsDivisibleByFive(num)) {
+            return "Buzz";
+        }
+        else {
+            return num.ToString();
+        }
+        
+    }
 
+    private bool IsDivisibleByThree(int num) {
+        return num % 3 == 0;
+    }
+
+    private bool IsDivisibleByFive(int num) {
+        return num % 5 == 0;
+    }
 }
